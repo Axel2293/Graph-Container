0ca7cb799e5b205d448c80403ec1e2ad75df7a24
axel <axelwa@outlook.com>
2022-11-27 22:44:20 -0600

    Minor bat change

--- compile.bat
+++ compile.bat
@@ -4,9 +4,7 @@ ECHO ----COMPILING main program DGRAPH
 
 cd "c:\Users\axelw\OneDrive - ITESO\Desktop\Graph\Graph-Container"
 
-gcc -c Graph.c Set\set.c 
-
-gcc main.c Graph.o set.o -o DGraph.exe
+gcc main.c Graph.c Set\set.c -o DGraph.exe
 
 ECHO ----TASK COMPLETED
 
