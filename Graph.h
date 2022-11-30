d84e32135b5386b4dde3b57138f0a8c4a48d5f15
Alv4rZ <juan.alvarez@iteso.mx>
2022-11-29 19:16:44 -0600

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -6,28 +6,32 @@
 #include <stddef.h>
 #include "List\list.h"
 
-typedef void * DATA;
+typedef struct nodeEdge * Edge;
 typedef struct graph * Graph;
 typedef struct vertex * Vertex;
 
-// Crea una nueva instancia de un contenedor DGraph. 
-Graph createGraph(size_t);
+// Crea una nueva instancia de un contenedor DGraph.
+Graph createGraph(size_t, size_t);
+
+Vertex createVertex(DATA, size_t, DATA, size_t);
 // Retorna el número de vértices que existen en el grafo.
 int sizeGraph(Graph);
 // Retornar true si en el contendor DG, existe una arista entre los vértices:  'x' y 'y'.
-bool adjacent(Graph, Vertex, Vertex);  
+bool adjacent(Graph, DATA, DATA);
 
 // #Retorna los datos que se encuentran en los vértices y que son vecinos del vértice x. 
-                                // Un vértice y es vecino de x, si existe una arista de x a y.*/
-List neighbors(Graph, Vertex);
+                                // Un vértice y es vecino de x, si existe una arista de x a y./
+List neighbors(Graph, DATA);
+//  #Agrega un nuevo vértice x, si no existe en el grafo DG. 
+bool addVertex(Graph, DATA, DATA);
+//  #Remueve el vértice x, si existe en el grafo DG.
+void removeVertex(Graph, DATA);
+//  #Agrega al grafo DG una arista z del vértice x al vértice y, si  no existe. 
+bool addEdge(Graph, DATA, DATA, DATA); 
 
-//void addVertex(DG, x) #Agrega un nuevo vértice x, si no existe en el grafo DG. 
+//void removeEdge(DG, x, y) #Remueve del grafo DG la arista del vértice x al vértice y, si existe.
 
-//void removeVertex(DG, x) #Remueve el vértice x, si existe en el grafo DG.
-
-//void addEdge(DG, x, y, z) #Agrega al grafo DG una arista z del vértice x al vértice y, si  no existe. 
-
-//void removeEdge(DG, x, y) #Remueve del grafo DG la arista del vértice x al vértice y, si existe. 
+DATA getEdgeLabel(Graph, DATA, DATA);
 
 
 #endif /* _GRAPH_H_ */
